8316331c37c435dfdc045b8ed3f9b02e7deba20d
Pranau Kumar <pranau97@gmail.com>
2018-04-07 13:04:07 +0530

    Change gunicorn config

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
-# This file contains the WSGI configuration required to serve up your
-# web application at http://<your-username>.pythonanywhere.com/
-# It works by setting the variable 'application' to a WSGI handler of some
-# description.
-#
-# The below has been auto-generated for your Flask project
+'''
+This file contains the WSGI configuration required to serve up your
+web application at http://<your-username>.pythonanywhere.com/
+It works by setting the variable 'application' to a WSGI handler of some
+description.
 
-from app import app as application
+The below has been auto-generated for your Flask project.
+'''
+
+# pylint: disable-msg=W0611
+
+from app import APP as application
